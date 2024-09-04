7e5742f0b375d367384e440dafcc20a75a12ee72
ryanschubert99 <71517074+ryanschubert99@users.noreply.github.com>
2024-09-04 13:25:27 -0400

    Create testCode.java

---
+++ src/testCode.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int x = 0;
+        for(int i = 0; i<5; i++){
+            x;
+        }
+        System.out.println(x)
+    }
+}
