ade52637863f085dcd3488d5e68152322c68b5b7
ryanschubert99 <71517074+ryanschubert99@users.noreply.github.com>
2024-09-04 13:53:32 -0400

    Update testCode.java

--- src/testCode.java
+++ src/testCode.java
@@ -1,11 +1,5 @@
 public class testCode {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        int x = 0;
-        for(int i = 0; i<5; i++){
-            x = x + i;
-        }
-        System.out.println(x);
+        System.out.println("hi");
     }
 }
