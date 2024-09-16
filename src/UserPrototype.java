b406a9ae6149570c720f56216ad734e555a19fdc
ryanschubert99 <71517074+ryanschubert99@users.noreply.github.com>
2024-09-16 15:19:47 -0400

    3 prototypes, and interfaces

---
+++ src/UserPrototype.java
@@ -0,0 +1,12 @@
+
+public class UserPrototype {
+	
+	public void prototypeCode(Database data) {
+		
+	public void sendValue() {}
+	
+	public int receiveValue() {
+		
+		return Database.request();
+	}
+}
