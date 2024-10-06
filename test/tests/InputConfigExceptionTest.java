91f832e0cf3f99876c3560468b462c10c6c9f2b3
ryanschubert99 <71517074+ryanschubert99@users.noreply.github.com>
2024-10-06 13:05:15 -0400

    Created a Unit Test for InputConfig
    Tests to make sure that valid values were passed through the input config to the computeRequest and then to the datastorage

---
+++ test/tests/InputConfigExceptionTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import src.InputConfigImplementation;
+
+public class InputConfigExceptionTest {
+
+  @Test
+  public void testInvalidInputTypeHandling() {
+    InputConfigImplementation inputConfig = new InputConfigImplementation();
+    inputConfig.setUserInputType();
+
+    // Check that only correct values were set to variables
+    assertTrue(inputConfig.getInputTypeValue() == 1 || inputConfig.getInputTypeValue() == 0);
+    assertTrue(inputConfig.getRows() > 0);
+    assertTrue(inputConfig.getColumns() > 0);
+    assertTrue(inputConfig.getMultiply() == 1 || inputConfig.getMultiply() == 0);
+  }
+}
