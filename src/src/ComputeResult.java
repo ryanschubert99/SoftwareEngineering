d3d41e2e7dc6d65d40d1de7217fa7426f26cc619
ryanschubert99 <71517074+ryanschubert99@users.noreply.github.com>
2024-10-21 14:51:36 -0400

    Checkstyle Fix

--- src/src/ComputeResult.java
+++ src/src/ComputeResult.java
@@ -1,35 +1,34 @@
 package src;
 
 public interface ComputeResult {
-	static ComputeResult SUCCESS = new ComputeResult() {
-        
-        @Override
-        public ComputeResultStatus getStatus() {
-            return ComputeResultStatus.SUCCESS;
-        }
-        
-        @Override
-        public String getFailureMessage() {
-            return "";
-        }
-    };
+  static ComputeResult SUCCESS = new ComputeResult() {
+    @Override
+    public ComputeResultStatus getStatus() {
+      return ComputeResultStatus.SUCCESS;
+    }
 
-	ComputeResultStatus getStatus();
-	String getFailureMessage();
-	
-	public static enum ComputeResultStatus {
-		SUCCESS(true),
-		INVALID_REQUEST(false),
-		FAILURE(false);
-		
-		private final boolean success;
-		
-		private ComputeResultStatus(boolean success) {
-			this.success = success;
-		}
-		
-		public boolean isSuccess() {
-			return success;
-		}
-	}
+    @Override
+    public String getFailureMessage() {
+      return "";
+    }
+  };
+
+  ComputeResultStatus getStatus();
+  String getFailureMessage();
+
+  public static enum ComputeResultStatus {
+    SUCCESS(true),
+    INVALID_REQUEST(false),
+    FAILURE(false);
+
+    private final boolean success;
+
+    private ComputeResultStatus(boolean success) {
+      this.success = success;
+    }
+
+    public boolean isSuccess() {
+      return success;
+    }
+  }
 }
--- src/src/ComputeResultImpl.java
+++ src/src/ComputeResultImpl.java
@@ -1,24 +1,23 @@
 package src;
 
 public class ComputeResultImpl implements ComputeResult {
-    
-    private final ComputeResultStatus status;
-    private final String failureMessage;
-    
-    public ComputeResultImpl(ComputeResultStatus status, String failureMessage) {
-        this.status = status;
-        this.failureMessage = failureMessage;
-    }
 
-    @Override
-    public ComputeResultStatus getStatus() {
-        return status;
-    }
+  private final ComputeResultStatus status;
+  private final String failureMessage;
 
-    @Override
-    public String getFailureMessage() {
-        return failureMessage;
-    }
+  public ComputeResultImpl(ComputeResultStatus status, String failureMessage) {
+    this.status = status;
+    this.failureMessage = failureMessage;
+  }
+
+  @Override
+  public ComputeResultStatus getStatus() {
+    return status;
+  }
+
+  @Override
+  public String getFailureMessage() {
+    return failureMessage;
+  }
 
 }
-
