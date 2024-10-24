c0bfd90d2586e887ad5d9cd50bfd722b0d07e58a
ryanschubert99 <71517074+ryanschubert99@users.noreply.github.com>
2024-10-24 14:52:46 -0400

    Checkstyle Fix

--- test/tests/TestUser.java
+++ test/tests/TestUser.java
@@ -1,28 +1,20 @@
 package tests;
 
 import java.io.File;
-import java.io.IOException;
-
 import src.ComputationCoordinatorImp;
 
-
 public class TestUser {
-	
-	// TODO 3: change the type of this variable to the name you're using for your
-	// User <-> ComputeEngine API; also update the parameter passed to the constructor
-	private final ComputationCoordinatorImp coordinator;
+  private final ComputationCoordinatorImp coordinator;
 
-	public TestUser(ComputationCoordinatorImp coordinator) {
-		this.coordinator = coordinator;
-	}
+  public TestUser(ComputationCoordinatorImp coordinator) {
+    this.coordinator = coordinator;
+  }
 
-	public void run(String outputPath) throws Exception {
-		char delimiter = ';';
-		String inputPath = "test" + File.separatorChar + "testInputFile.test";
-		
-		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
-		// run the compute job specified by inputPath, outputPath, and delimiter
-		coordinator.beginComputation(delimiter, inputPath, delimiter, delimiter, delimiter, delimiter, inputPath, delimiter);
-	}
+  public void run(String outputPath) throws Exception {
+    char delimiter = ';';
+    String inputPath = "test" + File.separatorChar + "testInputFile.test";
 
+    // Call the appropriate method on the coordinator to run the computation
+    coordinator.beginComputation(delimiter, inputPath, delimiter, delimiter, delimiter, delimiter, inputPath, delimiter);
+  }
 }
