2bb8cd44b599174acf7fa98c742898cda541009a
LourisA <147088420+Loura1928@users.noreply.github.com>
2024-11-14 13:33:00 -0500

    made input smaller so it passes gradle

--- test/tests/ComputeEngineIntegrationTest.java
+++ test/tests/ComputeEngineIntegrationTest.java
@@ -23,9 +23,9 @@ public class ComputeEngineIntegrationTest {
         // These values simulate what a user might input through a coordinator
         int inputType = 0; // Example: 0 for user input, 1 for file input
         String inputFileName = "input.txt";
-        int numberOfMatrices = 1000;
-        int rows = 100;
-        int columns = 100;
+        int numberOfMatrices = 3;
+        int rows = 3;
+        int columns = 3;
         int outputFileType = 1; // 0 = console output, 1 = file output
         String outputFileName = "output.txt";
         int outputOrComp = 1; // 1 for output, 0 for computation
